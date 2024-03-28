6582c80933c89ba8f30a8862b481039149834f30
Lucascontrucci <146679003+Lucascontrucci@users.noreply.github.com>
2024-03-27 21:33:54 -0300

    Delete SalesUnityAPI directory

--- SalesUnityAPI/src/main/java/com/salesunity/appsu/core/repositories/CompraRepository.java
+++
@@ -1,9 +0,0 @@
-package com.salesunity.appsu.core.repositories;
-
-import com.salesunity.appsu.core.entities.Compra;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompraRepository extends JpaRepository<Compra, Long> {
-}
